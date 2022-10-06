90787c19c0f711dfd2642d8f4ff58b9f11faf1a4
rigood <rigood@naver.com>
2022-10-06 21:52:19 +0900

    ✨ 동영상 업로드 시 동영상, 썸네일 미리보기 기능 추가

--- src/client/js/preview.js
+++ src/client/js/preview.js
@@ -1,21 +1,25 @@
 const file = document.getElementById("avatar");
 const file2 = document.getElementById("thumb");
+const file3 = document.getElementById("video");
 
 const handleFile = (event) => {
   const input = event.target;
 
-  // 이미지 파일이 있는 경우
   if (input.files && input.files[0]) {
-    // FileReader 인스턴스 생성
     const reader = new FileReader();
 
-    // 이미지 로드 되면
     reader.onload = (event) => {
-      const preview = document.querySelector(".preview");
-      preview.src = event.target.result;
+      let preview;
+      if (input.id === "video") {
+        preview = document.querySelector(".videoPreview");
+        preview.src = event.target.result;
+        preview.play();
+      } else {
+        preview = document.querySelector(".preview");
+        preview.src = event.target.result;
+      }
     };
 
-    // reader 이미지 읽기
     reader.readAsDataURL(input.files[0]);
   }
 };
@@ -26,4 +30,8 @@ if (file) {
 
 if (file2) {
   file2.addEventListener("change", handleFile);
+}
+
+if (file3) {
+  file3.addEventListener("change", handleFile);
 }
