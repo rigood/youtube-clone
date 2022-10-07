5fb8cdf88f9df3cce36ad38bf56c16246f84fec2
rigood <rigood@naver.com>
2022-10-08 02:01:49 +0900

    🐛 file-loadr 이미지 main.js에서 import

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,7 @@
 import "regenerator-runtime";
 import "../scss/style.scss";
+import "../img/404monkey.png";
+import "../img/default.jpg";
+import "../img/favicon.ico";
+import "../img/logo.png";
+import "../img/thumbnail.png";
