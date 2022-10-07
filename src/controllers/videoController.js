bbb9e6f8fe2dbb2642d410f015227d2c50452d85
rigood <rigood@naver.com>
2022-10-08 01:15:59 +0900

    🐛 edit 페이지 썸네일 업로드 required 삭제

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -99,7 +99,7 @@ export const postEdit = async (req, res) => {
 
   const thumbSize = thumb[0].size;
 
-  if (thumbSize > 5000000) {
+  if (thumb && thumbSize > 5000000) {
     return res.status(500).render("upload", { pageTitle, errorMsg: "5MB 이하 썸네일 이미지만 업로드 할 수 있습니다." });
   }
 
